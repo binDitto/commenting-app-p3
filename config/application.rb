f778144e0443a1eeeb1f0694340d876fdd160fa5
Bin Tran <sudocode.bin@gmail.com>
2017-02-12 02:44:24 -0800

    trying to add this

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,12 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
+
 module CommentingAppP3
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.assets.initialize_on_precompile = false
   end
 end
