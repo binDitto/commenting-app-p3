5eab5d6b2764e16ea4efbbb7069b85ee93065ce1
Bin Tran <sudocode.bin@gmail.com>
2017-02-12 03:55:32 -0800

    changed comment body length

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   def self.search(search)
     where("body LIKE ?", "%#{search}%")
-  
+
   end
-  validates :body, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :body, presence: true, length: { maximum: 300 }
 end
